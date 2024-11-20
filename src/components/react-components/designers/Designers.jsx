8a96dd0faa1eec8916fc7fdedc817bfa8c013997
k1ssa1 <samikissai.edu@gmail.com>
2024-11-20 20:10:39 +0100

    add animation to designer section

--- src/components/react-components/designers/Designers.jsx
+++ src/components/react-components/designers/Designers.jsx
@@ -1,5 +1,7 @@
 import styled from "./Designers.module.css"
 
+import anime from "animejs";
+
 const Designers = () => {
 
     const designerData = [
@@ -26,20 +28,93 @@ const Designers = () => {
         },
     ];
 
+    let hasAnimatedTitle = false; 
+    let hasAnimatedDescOne = false;
+    let hasAnimatedDescTwo = false;
+    let animatedCards = new Set();
+
+    document.addEventListener("scroll", () => {
+        const mainTitle = document.getElementById("main_title")
+        const descOne = document.getElementById("first_desc")
+        const descTwo = document.getElementById("second_desc")
+
+        if(!mainTitle) return;
+        if(!descOne) return;
+        if(!descTwo) return;
+
+        const mainTitlePos = mainTitle.getBoundingClientRect();
+        const descOnePos = descOne.getBoundingClientRect();
+        const descTwoPos = descTwo.getBoundingClientRect();
+
+
+        const cards = document.querySelectorAll(`.${styled.designer_card}`)
+        console.log(cards); 
+        cards.forEach((item, index) => {
+            const itemPos = item.getBoundingClientRect()
+            console.log(itemPos)
+            if(itemPos.top < innerHeight && itemPos.bottom > 0  && !animatedCards.has(item)){
+                anime({
+                    targets: item,
+                    opacity: [0, 1],
+                    duration: 4000,
+                    easing: "easeOutExpo",
+                    delay: index * 850
+                })
+                animatedCards.add(item); 
+            }
+        })
+
+        if(mainTitlePos.top < window.innerHeight && mainTitlePos.bottom > 0 && !hasAnimatedTitle){
+            hasAnimatedTitle = true;
+            anime({
+                targets: "#main_title",
+                opacity: [0, 1],
+                duration: 2000,
+                easing: "easeOutExpo",
+                delay: 600,
+                scale: 1.8
+            })
+        }
+
+        if(descOnePos.top < window.innerHeight && descOnePos.bottom > 0 && !hasAnimatedDescOne){
+            hasAnimatedDescOne = true;
+            anime({
+                targets: "#first_desc",
+                opacity: [0, 1],
+                duration: 4000,
+                easing: "easeOutExpo",
+                delay: 800,
+            })
+        }
+
+        if(descTwoPos.top < window.innerHeight && descTwoPos.bottom > 0 && !hasAnimatedDescTwo){
+            hasAnimatedDescTwo = true;
+            anime({
+                targets: "#second_desc",
+                opacity: [0, 1],
+                duration: 4000,
+                easing: "easeOutExpo",
+                delay: 1200,
+            })
+        }
+    })
+
+
+
     return ( 
         <div className={styled.main_container}>
             <div className={styled.container_header}>
-                <h2 className={styled.header_title}>Discover the Visionaries Behind the Art</h2>
+                <h2 className={styled.header_title} id="main_title">Discover the Visionaries Behind the Art</h2>
                 <ul className={styled.container_description_list}>
-                    <li className={styled.header_description_one}>Step into the world of fashion through the eyes of three visionary designers whose creativity knows no bounds. Each one has crafted a unique narrative, using their designs to challenge conventions and spark new conversations within the fashion world. From bold experimentation with color and structure to minimalist approaches that capture the beauty of simplicity, their work reflects a deep commitment to innovation and self-expression. These designers are redefining what fashion can represent, turning every piece into a statement of individuality.</li>
-                    <li className={styled.header_description_two}>As you explore their designs, you’ll uncover the stories that inspire them—stories that celebrate diversity, heritage, and the endless possibilities of creation. With a shared passion for pushing boundaries, they bring to life garments that go beyond the material. Their collections are a reflection of their values, philosophies, and artistic journeys, inviting you to join them in experiencing fashion as an evolving, living art form.</li>
+                    <li className={styled.header_description_one} id="first_desc">Step into the world of fashion through the eyes of three visionary designers whose creativity knows no bounds. Each one has crafted a unique narrative, using their designs to challenge conventions and spark new conversations within the fashion world. From bold experimentation with color and structure to minimalist approaches that capture the beauty of simplicity, their work reflects a deep commitment to innovation and self-expression. These designers are redefining what fashion can represent, turning every piece into a statement of individuality.</li>
+                    <li className={styled.header_description_two} id="second_desc">As you explore their designs, you’ll uncover the stories that inspire them—stories that celebrate diversity, heritage, and the endless possibilities of creation. With a shared passion for pushing boundaries, they bring to life garments that go beyond the material. Their collections are a reflection of their values, philosophies, and artistic journeys, inviting you to join them in experiencing fashion as an evolving, living art form.</li>
                 </ul>
             </div>
             <div className={styled.cards_container}>
                 {
                     designerData.map((designer) => {
                         return (
-                            <div key={designer.id} className={styled.designer_card}>
+                            <div key={designer.id} className={styled.designer_card} id="card">
                                 <div className={styled.designer_card_image_container}>
                                     <img src={designer.image} alt={designer.name} className={styled.designer_card_image}/>
                                 </div>
